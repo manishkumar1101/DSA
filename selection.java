e7b1cc7d2d9e2121c8f58e99a2cb4ce547e08fd5
manishkumar1101 <manishkumar.110104@gmail.com>
2024-05-16 13:52:05 +0530

    add selection sort

---
+++ selection.java
@@ -0,0 +1,35 @@
+// import java.util.Scanner;
+
+public class selection {
+    public static void main(String[] args) {
+      String s1="Hello";
+      String s2="Hello";
+      System.out.println(s1.equals(s2));
+    }
+//     public static int  swap(int a,int b){
+//         int temp=a;
+//         a=b;
+//         b=temp;
+//         return 
+//     }
+//     public static int selectionsort(int n,int arr){
+//         for(int i=0;i<n-2;i++){
+//             int min=i;
+//             for(int j=i;j<n-1;j++){
+//                 if(arr[i]<arr[min]){
+//                    min=j;
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int arr[]=new int[n];
+//             for(int i=0;i<n;i++){
+//                 arr[i]=sc.nextInt();
+//             }
+
+
+    }
